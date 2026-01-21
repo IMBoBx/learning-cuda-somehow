1eda76d8828c7ed4e01927a15b277bee04f8ccbf
IMBoBx <garvkundnaniyt@gmail.com>
2026-01-22 01:11:54 +0530

    changed matrix access notation

--- gridComputation.cu
+++ gridComputation.cu
@@ -1,14 +1,14 @@
 #include <cuda_runtime.h>
 
-#include <iostream>
 #include <cuda/cmath>
+#include <iostream>
 
-__global__ void computeGrid(float** A, int rows, int cols) {
+__global__ void computeGrid(float* A, int rows, int cols) {
     int x = threadIdx.x + blockDim.x * blockIdx.x;
     int y = threadIdx.y + blockDim.y * blockIdx.y;
 
     if (x < rows && y < cols) {
-        A[x][y] = sinf(x) + cosf(y);
+        A[x * cols + y] = sinf(x) + cosf(y);
     }
 }
 
@@ -26,31 +26,29 @@ int main(int argc, char** argv) {
         cols = std::atoi(argv[2]);
     }
 
-    float* h_data = (float*)malloc(rows * cols * sizeof(float));
-    float** h_A = (float**)malloc(rows * sizeof(float*));
+    int size = rows * cols * sizeof(float);
 
+    std::cout << "starting memory ops" << std::endl;
+    float* h_data = (float*)malloc(size);
     float* d_data = nullptr;
-    float** d_A = nullptr;
-    
-    cudaMalloc(&d_data, rows * cols * sizeof(float));
-    cudaMalloc(&d_A, rows * sizeof(float*));
-    
-    for (int i = 0; i < rows; i++) {
-        h_A[i] = d_data + i * cols;
-    }
-    cudaMemcpy(d_A, h_A, rows * sizeof(float*), cudaMemcpyHostToDevice);
-
-    dim3 threads(16, 16);
-    dim3 blocks(16, 16);
+    cudaMalloc(&d_data, size);
+    std::cout << "done" << std::endl;
 
     std::cout << "starting computation" << std::endl;
-    computeGrid<<<blocks, threads>>>(d_A, rows, cols);
+
+    dim3 threads(min(rows, 16), min(cols, 16));
+    dim3 blocks(cuda::ceil_div(rows, threads.x),
+                cuda::ceil_div(cols, threads.y));
+
+    computeGrid<<<blocks, threads>>>(d_data, rows, cols);
     cudaDeviceSynchronize();
+
     std::cout << "done" << std::endl;
-    
-    cudaMemcpy(h_data, d_data, rows * cols * sizeof(float), cudaMemcpyDeviceToHost);
-    
-    
+
+    std::cout << "starting memory ops" << std::endl;
+    cudaMemcpy(h_data, d_data, size, cudaMemcpyDeviceToHost);
+    std::cout << "done" << std::endl;
+
     for (int i = 0; i < rows && rows * cols <= 100; i++) {
         for (int j = 0; j < cols; j++) {
             printf("%-2.5f   ", h_data[i * cols + j]);
@@ -58,9 +56,6 @@ int main(int argc, char** argv) {
         std::cout << std::endl;
     }
 
-
-    cudaFree(d_A);
     cudaFree(d_data);
-    free(h_A);
     free(h_data);
-} 
+}
